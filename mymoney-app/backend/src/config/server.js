3775621345b60786f86c2e203cb2f7b2b091230b
Ironi Júnior Medina <ironimedina@gmail.com>
2018-07-11 08:53:58 -0300

    Add middleware to convert URL parameters

--- mymoney-app/backend/src/config/server.js
+++ mymoney-app/backend/src/config/server.js
@@ -3,12 +3,16 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const allowCors = require('./cors')
+const queryParser = require('express-query-int')
 
 const server = express()
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
+
+//Middleware to convert the 'skip' and 'limit' URL parameters to Number
+server.use(queryParser())
 
 server.listen(port, function() {
     console.log(`BACKEND is running at ${port}`)
