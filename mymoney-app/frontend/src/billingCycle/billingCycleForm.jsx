e66a723d880a10bac6314b66cbb95a700e5fca48
Ironi Júnior Medina <ironimedina@gmail.com>
2018-07-20 15:58:17 -0300

    Improves on Form layout

--- mymoney-app/frontend/src/billingCycle/billingCycleForm.jsx
+++ mymoney-app/frontend/src/billingCycle/billingCycleForm.jsx
@@ -1,17 +1,24 @@
 import React, { Component } from 'react'
 import { reduxForm, Field } from 'redux-form'
 
+import labelAndInput from '../common/form/labelAndInput';
+
 class BillingCycleForm extends Component {
     
     render() {
         const { handleSubmit } = this.props
-        
+
         return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>
-                    <Field name='name' component='input' />
-                    <Field name='month' component='input' />
-                    <Field name='year' component='input' />
+                    <Field name='name' component={labelAndInput}
+                        label='Name' placeholder='Billing cycle name' cols='12 4' />
+
+                    <Field name='month' component={labelAndInput} type='number'
+                        label='Month' placeholder='Billing cycle month' cols='12 4'  />
+
+                    <Field name='year' component={labelAndInput} type='number'
+                        label='Year' placeholder='Billing cycle year' cols='12 4' />
                 </div>
                 <div className='box-footer'>
                     <button type='submit' className='btn btn-primary'>Submit</button>
