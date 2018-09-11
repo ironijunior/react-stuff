dc88e693bb4438ff4efe3cba7b6bb0a922e7a25e
Ironi Júnior Medina <ironimedina@gmail.com>
2018-09-11 16:27:37 -0300

    Added authentication by token; Added routers for login/signup/validate

--- mymoney-app/backend/src/config/routes.js
+++ mymoney-app/backend/src/config/routes.js
@@ -1,12 +1,28 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function(server) {
 
-    //Base URL for all routes
-    const router = express.Router()
-    server.use('/api', router)
+    /*
+     * Protected Route by JWT Token
+     */
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+
+    protectedApi.use(auth)
 
     //Define BillingCycle routes
     const billingCycleService = require('../api/billingCycle/billingCycleService')
-    billingCycleService.register(router, '/billingCycles')
+    billingCycleService.register(protectedApi, '/billingCycles')
+
+    /*
+     * Open Route
+     */
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/authService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/valideToken', AuthService.validateToken)
 }
