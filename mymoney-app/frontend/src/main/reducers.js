20bcce296bf2b46a6207321eca8c33a9b7903dc5
Ironi Jr. Medina <ironimedina@gmail.com>
2018-09-11 21:29:31 -0300

    Created reduce and action to authentication

--- mymoney-app/frontend/src/main/reducers.js
+++ mymoney-app/frontend/src/main/reducers.js
@@ -4,12 +4,14 @@ import { reducer as toastrReducer } from 'react-redux-toastr'
 
 import TabReducer from '../common/tab/tabReducer'
 import BillingCycleReducer from '../billingCycle/billingCycleReducer'
+import AuthReducer from '../auth/authReducer'
 
 const rootReducer = combineReducers({
     tab: TabReducer,
     billingCycle: BillingCycleReducer,
     form: formReducer,
-    toastr: toastrReducer
+    toastr: toastrReducer,
+    auth: AuthReducer
 })
 
 export default rootReducer
